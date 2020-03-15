c1f65453a8373b774967a04b003e7e376bd1ee41
Ascalonic <varghesemathaime@gmail.com>
2020-03-15 21:36:25 +0530

    bugfix

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-try:
-  # %tensorflow_version only exists in Colab.
-  %tensorflow_version 2.x
-except Exception:
-  pass
 import tensorflow as tf
 
 # You'll generate plots of attention in order to see which parts of an image
