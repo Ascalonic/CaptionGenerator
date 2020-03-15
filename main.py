3d1d0832278e0ae31a1ea5f5f8342db867d767c3
Ascalonic <varghesemathaime@gmail.com>
2020-03-15 21:34:44 +0530

    limit train size

--- main.py
+++ main.py
@@ -175,6 +175,33 @@ else:
   PATH = os.path.abspath('.') + image_folder
 
 
+# Read the json file
+with open(annotation_file, 'r') as f:
+    annotations = json.load(f)
+
+# Store captions and image names in vectors
+all_captions = []
+all_img_name_vector = []
+
+for annot in annotations['annotations']:
+    caption = '<start> ' + annot['caption'] + ' <end>'
+    image_id = annot['image_id']
+    full_coco_image_path = PATH + 'COCO_train2014_' + '%012d.jpg' % (image_id)
+
+    all_img_name_vector.append(full_coco_image_path)
+    all_captions.append(caption)
+
+# Shuffle captions and image_names together
+# Set a random state
+train_captions, img_name_vector = shuffle(all_captions,
+                                          all_img_name_vector,
+                                          random_state=1)
+
+# Select the first 30000 captions from the shuffled set
+num_examples = 30000
+train_captions = train_captions[:num_examples]
+img_name_vector = img_name_vector[:num_examples]
+
 image_model = tf.keras.applications.InceptionV3(include_top=False,
                                                 weights='imagenet')
 new_input = image_model.input
