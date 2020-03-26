b5b870b180c9840390f7fad9fc7e73a434519f87
Ascalonic <varghesemathaime@gmail.com>
2020-03-27 00:38:44 +0530

    tokenizer changes

--- test.py
+++ test.py
@@ -124,9 +124,6 @@ top_k = 5000
 tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
                                                   oov_token="<unk>",
                                                   filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
-tokenizer.fit_on_texts(train_captions)
-train_seqs = tokenizer.texts_to_sequences(train_captions)
-
 tokenizer.word_index['<pad>'] = 0
 tokenizer.index_word[0] = '<pad>'
 
